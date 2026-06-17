d8b2aedb3b73ee3e74060c7c2a2359f14759d4b1
Rsr07k <rsr07k@gmail.com>
2026-06-18 00:38:31 +0530

    Till Now i have completed Calculator working part only remains now Exception handlingand resposiveness of the website and testing too.Thankyou

--- script.js
+++ script.js
@@ -1,42 +1,120 @@
 let btns=document.querySelectorAll(".buttons");
 let ans=document.querySelector(".ans")
 
+
+//to convert string into array so that we can separate num and operators during postfix conversion
+function gettokens(x){
+    let token=[];
+    let str="";
+   for(let i=0;i<x.length;i++){
+    
+    if(x[i]=='+' || x[i]=='-' || x[i]=='*' || x[i]=='/' || x[i]=='(' || x[i]==')' ){
+        if(str!=="")
+        token.push(str);
+        
+        token.push(x[i]);
+        str="";
+
+    }
+    else {
+        str+=x[i];
+    }
+   }
+   token.push(str);
+   return token;
+
+}
+
+//get postfix expression
+function getpostfix(x){
+    let st=[];
+    let postfix=[];
+    let p=new Map();
+    p.set("+",1);
+    p.set("-",1);
+    p.set("*",2);
+    p.set("/",2);
+    
+    for(let i=0;i<x.length;i++){
+        if(!isNaN(Number(x[i]))){
+            postfix.push(x[i]);
+        }
+        else{
+            if(st.length==0)st.push(x[i]);
+            else if(x[i]=="(" || p.get(x[i])>p.get(st[st.length-1]))st.push(x[i]);
+            else if(x[i]==')'){
+                while(st[st.length-1]!=='('){
+                    postfix.push(st[st.length-1]);
+                    st.pop();
+                }
+                st.pop();
+            }
+            else {
+                while(st.length>0 && p.get(x[i])<=p.get(st[st.length-1])){
+                    postfix.push(st[st.length-1]);
+                    st.pop();
+                }
+                st.push(x[i]);
+            }
+            
+        }
+    }   
+    while(st.length>0){
+        postfix.push(st[st.length-1]);
+        st.pop();
+
+    } 
+    return postfix;
+    
+}
+
+    const evaluate=(curr)=>{
+    let st=[];
+    
+    for(let i=0;i<curr.length;i++){
+    if(!isNaN(Number(curr[i]))){
+        st.push(Number(curr[i]));
+    }
+    else {
+        let b=st.pop();
+        let a =st.pop();
+        if(curr[i]=="+")st.push(a+b);
+        else if(curr[i]=='-')st.push(a-b);
+        else if(curr[i]=='*')st.push(a*b);
+        else st.push(a/b);
+        
+    }
+    }
+    return st[0];
+    }
+
 btns.forEach((btn)=>{
 btn.addEventListener("click",function(){
     let x=btn.textContent;
     let curr=ans.textContent;
     if(x=="AC")ans.textContent="";
-    else if(x=="DE" && ans.textContent.length>0)ans.textContent=ans.textContent.slice(0,-1);
+    else if(x=="DE" ){
+        if( ans.textContent.length>0)
+        ans.textContent=ans.textContent.slice(0,-1);
+    }
     else if(x!=="="){
         ans.textContent+=x;
     }
     else {
-        let first="";
-        let second="";
-        let symbol="";
-        for(let i=0;i<curr.length;i++)
-        {   
-            if(i==0 && curr[0]=='-'){
-                first+=curr[0];
-            }
-            else if(  (curr.charCodeAt(i)<=57 && curr.charCodeAt(i)>=48)){
-                first+=curr[i];
-            }
-            else {
-                second=first;
-                first="";
-                symbol=curr[i];
-            }
-            
-        }
-        if(symbol=="*")
-        ans.textContent=Number(second)*Number(first);
-        else if(symbol=="/")
-        ans.textContent=Number(second)/Number(first);
-        else if(symbol=="+")
-        ans.textContent=Number(second)+Number(first);
-        else if(symbol=="-")
-        ans.textContent=Number(second)-Number(first);
+        curr=gettokens(curr);
+        console.log(curr);
+        curr=getpostfix(curr);
+        curr=evaluate(curr);
+        curr=curr.toPrecision(5);
+        ans.textContent=curr;
+    
+        
+        
+        
+        
+        
+
+        
         
     }
 
